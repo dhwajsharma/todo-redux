48f9058f48ee6306acd065ea2f91fa3bb3a9c394
Dhwaj Sharma <dhwajsharma10@gmail.com>
2022-03-11 12:05:23 +0530

    done setting up redux

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -1,10 +1,23 @@
 import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { saveTodo } from "../../features/todoSlice";
 import "./Input.css";
 
 const Input = () => {
   const [input, setInput] = useState("");
+  const dispatch = useDispatch();
 
-  const addTodo = () => {};
+  const addTodo = () => {
+    console.log(`Adding ${input} `);
+
+    dispatch(
+      saveTodo({
+        item: input,
+        done: false,
+        id: Date.now(),
+      })
+    );
+  };
 
   return (
     <div className="input">
---
+++ src/features/todoSlice.js
@@ -0,0 +1,19 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  todoList: [],
+};
+
+const todoSlice = createSlice({
+  name: "todos",
+  initialState,
+  reducers: {
+    saveTodo: (state, action) => {
+      state.todoList.push(action.payload);
+    },
+  },
+});
+
+export const { saveTodo } = todoSlice.actions;
+
+export default todoSlice.reducer;
