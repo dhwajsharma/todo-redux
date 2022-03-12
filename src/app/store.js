02ecdfc467a1051c7a1a53b84325af445ccfc23d
Dhwaj Sharma <dhwajsharma10@gmail.com>
2022-03-12 09:35:28 +0530

    Implemented redux persist

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,31 @@
 import { configureStore } from "@reduxjs/toolkit";
 import todoReducer from "../features/todoSlice";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+const persistConfig = {
+  key: "root",
+  version: 1,
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, todoReducer);
 
 export const store = configureStore({
-  reducer: {
-    todos: todoReducer,
-  },
+  reducer: { todos: persistedReducer },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
